bff8fc62a9e2869bdbb98173881dccb70254fdcc
abruu <abrin199@gmail.com>
2025-04-05 11:52:53 +0530

    chnages

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,6 @@ npm run build
 
 # Use exact path to systemctl
 sudo /usr/bin/systemctl reload nginx
+#!/bin/bash
+echo "Running deployment..."
+sudo /bin/systemctl restart nginx || sudo /bin/systemctl start nginx
