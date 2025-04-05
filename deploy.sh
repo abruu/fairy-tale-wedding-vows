9a219c6ec2402b9a105936d1e2db0d2cd2f75e29
abruu <abrin199@gmail.com>
2025-04-05 11:33:15 +0530

    cchange

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Navigate to the project directory
 cd /var/www/fairy-tale-wedding-vows
 
-# Optional: Set correct ownership and permissions
-sudo chown -R www-data:www-data dist
-sudo chmod -R 755 dist
+# Ensure permissions
+sudo /bin/chown -R www-data:www-data dist
+sudo /bin/chmod -R 755 dist
 
 # Reload nginx
 sudo /usr/bin/systemctl reload nginx
